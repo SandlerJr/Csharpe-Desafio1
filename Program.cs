6f5e6c3e0fa8fa28dd2f75d3523031af08f9869e
Sandler GMJ <sandlerjunin@gmail.com>
2026-03-16 10:55:11 -0300

    style: adicionar comentário identificando atividade 1 e ajustar indentação do código

--- Program.cs
+++ Program.cs
@@ -1,27 +1,30 @@
 ﻿    using System;
     class Program
     {
-        static void Main(string[] args)
-        {
-            int nota = -1;
-            while (nota == -1) {
-                Console.WriteLine("Digite sua nota media(numeros inteiros de 0 a 10):");
-                nota = int.TryParse(Console.ReadLine(), out int result) ? result : -1;
-                if (nota > 10 || nota < 0)
-                {
+        //Atividade 1: Escreva um programa que solicite ao usuário sua nota média (números inteiros de 0 a 10) e exiba se ele foi aprovado ou reprovado. O programa deve validar a entrada do usuário, garantindo que seja um número inteiro dentro do intervalo permitido. Se a entrada for inválida, o programa deve solicitar novamente até que uma entrada válida seja fornecida.
+            static void Main(string[] args)
+            {
+                int nota = -1;
+                while (nota == -1) {
+                    Console.WriteLine("Digite sua nota media(numeros inteiros de 0 a 10):");
+                    nota = int.TryParse(Console.ReadLine(), out int result) ? result : -1;
+                    if (nota > 10 || nota < 0)
+                    {
 
-                    Console.WriteLine("Nota inválida.");
-                    nota = -1;
-                } else {
-                    if (nota >= 6)
-                    {
-                        Console.WriteLine("Aprovado");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Reprovado");
+                        Console.WriteLine("Nota inválida.");
+                        nota = -1;
+                    } else {
+                        if (nota >= 6)
+                        {
+                            Console.WriteLine("Aprovado");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Reprovado");
+                        }
                     }
                 }
-            }
+
+
         }
     }
