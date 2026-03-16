b6576e088b82ce47f9a5450f03260ead7dbb3ea5
Sandler GMJ <sandlerjunin@gmail.com>
2026-03-16 10:49:06 -0300

    feat: implementar validação de nota com loop e TryParse

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota = -1;
+            while (nota == -1) {
+                Console.WriteLine("Digite sua nota media(numeros inteiros de 0 a 10):");
+                nota = int.TryParse(Console.ReadLine(), out int result) ? result : -1;
+                if (nota > 10 || nota < 0)
+                {
+
+                    Console.WriteLine("Nota inválida.");
+                    nota = -1;
+                } else {
+                    if (nota >= 6)
+                    {
+                        Console.WriteLine("Aprovado");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Reprovado");
+                    }
+                }
+            }
+        }
+    }
